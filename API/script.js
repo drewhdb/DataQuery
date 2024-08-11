46b82e6ae651fa5b35119594d7f7293c553381fc
Andrew Horn de Borba <Andrewhorndeborba@gmail.com>
2024-08-11 01:05:33 -0300

    validação ok, falta consulta e solicitação

--- API/script.js
+++ API/script.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 
 // CRIAÇÃO DAS VARIAVEIS
-let deviceId, connection, clientes = [],
+let deviceId, connection,
 bloqueioDevice, bloqueioSolicitacao, novaSolicitacao, repeteSolicitacao;
 
 // CONEXÃO COM O BANCO PRINCIPAL
@@ -24,32 +24,36 @@ const dbConfig = {
 app.get('/alexa/script.js', async (req, res) => {
     /* -- VALIDAÇÃO DO DEVICE ID -- */
     deviceId = req.query.deviceId;
-    if (!deviceId || deviceId.length < 250) {
-        return res.status(200).send({'return' : 'Parâmetro "deviceId" é necessário.'});
+    if (!deviceId || deviceId.length < 200) {
+        return res.status(200).send({'return' : 'Parâmetro deviceId é necessário.'});
     }
 
-    /* -- DECLARAÇÃO DOS RETORNOS -- */
-    pegaRetornos();
-
-    /* -- VALIDAÇÃO -- */
-    // QUERY DEVICE
-	let queryDevice = 'SELECT * from dvc where deviceid = \'' + deviceId + '\';';
-	
-	try {
+    try {
         connection = await mysql.createConnection(dbConfig);
-
-		cli_dvc = await connection.execute(queryDevice);
-        cli_dvc.pop();
-
-        connection.end();        
     } catch (error) {
-        return res.status(200).send({'return' : 'Erro ao se conectar ao banco de dados principal.' + error});
+        console.log('Erro ao se conectar ao banco de dados principal.' + error);
+        return res.status(200).send({'return' : 'Erro ao se conectar ao banco de dados principal.'});
     }
     
+    /* -- DECLARAÇÃO DOS RETORNOS -- */
+    await pegaRetornos();
+
+    /* -- VALIDAÇÃO -- */
+    // Pegar dados vinculados a esse device
+	let queryDevice = 'SELECT * from dvc where deviceid = \'' + deviceId + '\';';	
+    let clientes_do_device = await connection.execute(queryDevice);
+    clientes_do_device.pop();
+
+    
     // testar se o Device existe no DVC
-    if(cli_dvc[0].length > 0){
-        if (validacao()){
+    if(clientes_do_device[0].length > 0){
+        log('chamado', 'Inicio do chamado. Iniciando validação', 1);
+        if (await validacao(clientes_do_device[0])){
+            log('validado', 'Dados validados, iniciando consulta', 2);
             consulta();
+        } else {
+            log('retorno','não foi encontrado nenhum cliente válido', 3);
+            return res.status(200).send({'return' : bloqueioDevice});
         }
     // testar se existe solicitação
     } else {
@@ -57,74 +61,67 @@ app.get('/alexa/script.js', async (req, res) => {
     }
 });
 
-function consulta(){
-    log('consulta', 'inicio da consulta', 1);
-
-}
-
-function validacao(){
-    // filtra clientes ativos
-    cli_dvc[0].forEach(element => {
-        if (element['bloqueado'] == 1){
-            log('device_cliente', 'device bloqueado para o cliente ' + element['cliente'], 2);
-        } else {
-            clientes.push(element);
-        }        
-    });
-    // testa se possui cliente valido
-    if (clientes.length === 0){
-        log('retorno','não foi encontrado nenhum device válido', 3);
-        return res.status(200).send({'return' : bloqueioDevice});
-    }
+async function validacao(clientes_do_device){
     //testa clientes
-    for (let i = 0; i < clientes.length; i++) {
-        let queryCliente = 'Select * from cli where cli.id = \'' + clientes[i]['cliente'] + '\';'; 
-        let queryGrupos = 'Select * from grp where grp.cliente = \'' + clientes[i]['cliente'] + '\';'; 
-        try {
-            connection = mysql.createConnection(dbConfig);
-            
-            cliente = connection.execute(queryCliente);
-            cliente = cliente[0][0];
-            
-            if(cliente['bloqueado'] == 1){
-                log('consulta', 'cliente ' + cliente['cliente'] + ' bloqueado.', 2);
-            } else if (testarConexao(cliente) == 0){
-                log('falha na conexão', 'cliente ' + cliente['cliente'] + ' sem acesso ao banco de dados.', 2);
-            } else {
-                let grp_cli = connection.execute(queryGrupos);
-                grp_cli.pop();
+    let clientes = [];
+    for (let i = 0; i < clientes_do_device.length; i++) {
+        let queryCliente = 'Select * from cli where cli.id = \'' + clientes_do_device[i]['cliente'] + '\';'; 
+        let cliente = await connection.execute(queryCliente);
+        cliente = cliente[0][0];
 
-                // testa grupos
-                if(grp_cli[0].length > 0){
-                    // filtra grupos ativos
-                    let grupos = [];
-                    grp_cli[0].forEach(element => {
-                        if (element['ativo'] == 0){
-                            log('consulta cliente ' + element['cliente'], 'grupo inativo ' + element['grupo'], 2);
-                        } else {
-                            grupos.push(element);
-                        }
-                    });
-                                           
-                    if (grupos.length === 0){
-                        log('consulta cliente ' + cliente['cliente'], 'Todos os grupos estão inativos', 2);
-                        clientes = clientes.filter(item => item['cliente'] !== cliente['id']);
-                        if (clientes.length === 0){
-                            log('retorno','não foi encontrado nenhum device válido', 3);
-                            return res.status(200).send({'return' : bloqueioDevice});
-                        }
-                    } else {
+        if(cliente['bloqueado'] == 1){
+            log('consulta', 'cliente ' + cliente['cliente'] + ' bloqueado.', 2);
+        } else if (await testarConexao(cliente) == 0){
+            log('falha na conexão', 'cliente ' + cliente['cliente'] + ' sem acesso ao banco de dados.', 2);
+        } else {
+            let queryGrupos = 'Select * from grp where grp.cliente = \'' + clientes_do_device[i]['cliente'] + '\';'; 
+            let grupos_do_cliente = await connection.execute(queryGrupos);
+            grupos_do_cliente.pop();
+            grupos_do_cliente = grupos_do_cliente[0];
+            // testa grupos
+            if(grupos_do_cliente.length > 0){
+                // filtra grupos ativos
+                let grupos = [];
+                for (let g = 0; g < grupos_do_cliente.length; g++) {
+                    if(grupos_do_cliente[g]['ativo'] == 1){
+                        let queryQueryes = 'Select * from qry where qry.grupo = \'' + grupos_do_cliente[g]['id'] + '\' and qry.cliente = \'' + clientes_do_device[i]['cliente'] + '\';'; 
+                        let queryes_do_grupo = await connection.execute(queryQueryes);
+                        queryes_do_grupo.pop();
+                        queryes_do_grupo = queryes_do_grupo[0];
                         // testa queryes
+                        if(queryes_do_grupo.length > 0){
+                            let queryes = [];
+                            for (let q = 0; q < queryes_do_grupo.length; q++) {
+                                if(queryes_do_grupo[q]['ativo'] == 1){
+                                    queryes.push(queryes_do_grupo[q]);
+                                }
+                            }
+                            if (queryes.length > 0) {
+                                grupos.push(grupos_do_cliente[g]);
+                            }
+                        }
                     }
-                } else{
-                    log('retorno', 'nenhum grupo para o cliente ' + cliente['cliente'], 3);
                 }
+                if (grupos.length === 0){
+                    log('dados cliente ' + cliente['cliente'], 'Todos os grupos estão inativos', 2);
+                } else {
+                    clientes.push(cliente);
+                }
+            } else{
+                log('retorno', 'nenhum grupo para o cliente ' + cliente['cliente'], 3);
             }
-            connection.end();        
-        } catch (error) {
-            return res.status(200).send({'return' : 'Erro ao se conectar ao banco de dados principal.' + error});
         }
     }
+    console.log(clientes);
+    // testa se possui cliente valido
+    if (clientes.length > 0){
+        return true;
+    }
+    return false;
+}
+
+function consulta(){
+    
 }
 
 async function pegaRetornos() {	
@@ -135,60 +132,36 @@ async function pegaRetornos() {
     let queryRepeteSolicitacao = 'SELECT texto as repeteSolicitacao FROM rtn where chave = \'repeteSolicitacao\';';
 
     // PEGA RETORNOS NO BANCO DE DADOS PRINCIPAL
-    try {
-        connection = await mysql.createConnection(dbConfig);
-
-        resposta = await connection.execute(queryBloqueioDevice);
-		bloqueioDevice = resposta[0][0]['bloqueioDevice'];
-		resposta = await connection.execute(queryBloqueioSolicitacao);
-		bloqueioSolicitacao = resposta[0][0]['bloqueioSolicitacao'];
-		resposta = await connection.execute(queryNovaSolicitacao);
-		novaSolicitacao = resposta[0][0]['novaSolicitacao'];
-		resposta = await connection.execute(queryRepeteSolicitacao);
-		repeteSolicitacao = resposta[0][0]['repeteSolicitacao'];
-
-		connection.end();        
-    } catch (error) {
-        return res.status(200).send({'return' : 'Erro ao se conectar ao banco de dados principal.' + error});
-    }
+    resposta = await connection.execute(queryBloqueioDevice);
+    bloqueioDevice = resposta[0][0]['bloqueioDevice'];
+    resposta = await connection.execute(queryBloqueioSolicitacao);
+    bloqueioSolicitacao = resposta[0][0]['bloqueioSolicitacao'];
+    resposta = await connection.execute(queryNovaSolicitacao);
+    novaSolicitacao = resposta[0][0]['novaSolicitacao'];
+    resposta = await connection.execute(queryRepeteSolicitacao);
+    repeteSolicitacao = resposta[0][0]['repeteSolicitacao'];
 }
 
 async function log(chave, texto, status) {
 	let query = 'INSERT INTO log (deviceId, horario, chave, texto, status) values (\'' + deviceId + '\', now(), \'' + chave + '\', \'' + texto + '\', \'' + status + '\');';
-    try {
-        const connection = await mysql.createConnection(dbConfig);
-        await connection.execute(query);
-        await connection.end();
-        
-    } catch (error) {
-        console.error('Erro ao inserir log.', error);
-    }
+    await connection.execute(query);        
 }
 
 async function testarConexao(cliente) {
-    console.log('foi a conexão');
-    let status = 0; // Valor padrão é 0 (erro)
     try {
-      // Cria uma conexão com o banco de dados usando Promises
-      const conexao = await mysql.createConnection({
+      const conexaoCliente = await mysql.createConnection({
         host: cliente.host,
         user: cliente.user,
         password: cliente.pass,
         port: cliente.port
       });
-  
-      // Se conectar sem erros, define o status como 1
-      status = 1;
-  
-      // Fecha a conexão
-      await conexao.end();
+      await conexaoCliente.end();
+      
     } catch (err) {
-      // Se ocorrer um erro, o status permanece como 0
       console.error('Erro ao conectar:', err);
+      return 0
     }
-  
-    // Retorna o status
-    return status;
+    return 1;
 }
 
 http.createServer(app).listen(port, () => {
